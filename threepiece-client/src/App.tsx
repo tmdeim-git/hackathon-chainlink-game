a2c7b672a3793a60181244dea2fb535121395334
RaulSima <77171346+RaulSima@users.noreply.github.com>
2024-05-10 00:55:44 -0400

    idk

--- threepiece-client/src/App.tsx
+++ threepiece-client/src/App.tsx
@@ -1,15 +1,14 @@
-import './App.css'
-import Game from './game/Game'
-import Connect from './auth/Connect'
+import "./App.css";
+import Game from "./game/Game";
+import Connect from "./auth/Connect";
 
 function App() {
-
   return (
-    <>
+    <div className="app">
       <Game />
       <Connect />
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
