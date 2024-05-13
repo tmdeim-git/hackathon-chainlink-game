db1d01cf9cc428055b58bf2accfc15dea21c4ece
TheVortex8 <47262090+TheVortex8@users.noreply.github.com>
2024-05-13 02:21:36 -0400

    reduced number of tiles (easier nft creation)

--- threepiece-client/src/game/client.ts
+++ threepiece-client/src/game/client.ts
@@ -4,8 +4,8 @@ export function getGameTiles(): GameTile[] {
   const result: GameTile[] = [];
   let size: number = 75;
 
-  for (let i = 0; i < 55; i++) {
-    for (let j = 0; j < 75; j++) {
+  for (let i = 0; i < 11; i++) {
+    for (let j = 0; j < 15; j++) {
       result.push(new GameTile(i, j, size, i * 10 + j));
     }
   }
