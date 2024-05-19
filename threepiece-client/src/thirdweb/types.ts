fa46201826ae51936c31cac2f107e532efec63eb
RaulSima <77171346+RaulSima@users.noreply.github.com>
2024-05-18 22:49:58 -0400

    formated types

--- threepiece-client/src/thirdweb/types.ts
+++ threepiece-client/src/thirdweb/types.ts
@@ -1,49 +1,66 @@
 import { NFT } from "thirdweb";
 
 export type LandNFT = NFT & {
-    metadata: {
-        attributes?: LandNFTAttributes
-    }
-}
+  metadata: {
+    attributes?: LandNFTAttributes;
+  };
+};
 
 export enum LandEvent {
-    None = "none",
-    Raining = "raining"
+  None = "none",
+  Raining = "raining"
 }
 
 export type LandNFTAttributes = [
-    {
-        readonly trait_type: 'id',
-        value: number
-    },
-    {
-        readonly trait_type: 'resources',
-        value: Resource[]
-    },
-    {
-        readonly trait_type: 'event',
-        value: LandEvent
-    },
-]
+  {
+    readonly trait_type: "id";
+    value: number;
+  },
+  {
+    readonly trait_type: "resources";
+    value: Resource[];
+  },
+  {
+    readonly trait_type: "event";
+    value: LandEvent;
+  }
+];
 
 export interface Land {
-    id: number
-    resources: Resource[]
-    event?: LandEvent
-    nft?: LandNFT
-    ownerAddress?: string
+  id: number;
+  resources: Resource[];
+  event?: LandEvent;
+  nft?: LandNFT;
+  ownerAddress?: string;
 }
 
 export interface Owner {
-    address: string
-    inventory?: Item[]
+  address: string;
+  inventory?: Item[];
 }
 
 export abstract class Item {
-    name: string
+  name: string;
 }
 
 export enum Resource {
+  Sand = "sand",
+  Seawater = "seawater",
+  Water = "water",
+  Wood = "wood",
+  Soil = "soil",
+  Ore = "ore"
+}
+
+/*
+export interface Resource {
+    resourceType: ResourceType,
+    ProductionRate: number,
+    MaximumAmmount: number,
+    CurrentAmmount: number
+}
+
+export enum ResourceType {
     Sand = 'sand',
     Seawater = 'seawater',
     Water = 'water',
@@ -51,32 +68,35 @@ export enum Resource {
     Soil = 'soil',
     Ore = 'ore',
 }
+*/
 
 export interface MetadataAttributes {
-    trait_type: string
-    value: any
+  trait_type: string;
+  value: any;
 }
 
 export function isValidLand(land: any): land is Land {
-    const resources = land.resources;
-    const landEvent = land.event;
+  const resources = land.resources;
+  const landEvent = land.event;
 
-    return isValidEnumArray(resources, Resource) && isValidEnumValue(landEvent, LandEvent)
+  return (
+    isValidEnumArray(resources, Resource) &&
+    isValidEnumValue(landEvent, LandEvent)
+  );
 }
 
 // Generic type predicate function for array of enums
 export function isValidEnumArray<T extends string>(
-    arr: string[],
-    enumValues: Record<string, T>
+  arr: string[],
+  enumValues: Record<string, T>
 ): arr is T[] {
-    return arr.every(value => Object.values(enumValues).includes(value as T));
+  return arr.every((value) => Object.values(enumValues).includes(value as T));
 }
 
 // Generic type predicate function for single enum value
 export function isValidEnumValue<T extends string>(
-    value: string,
-    enumValues: Record<string, T>
+  value: string,
+  enumValues: Record<string, T>
 ): value is T {
-    return Object.values(enumValues).includes(value as T);
+  return Object.values(enumValues).includes(value as T);
 }
-
